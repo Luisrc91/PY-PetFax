47b59771601d95e797dba877fd09f2d7c85101f4
Luis <luisrc91@live.com>
2023-06-11 23:15:21 -0400

    added show and facts

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,23 +1,21 @@
 from flask import Flask
 
 def create_app():
-
     app = Flask(__name__)
 
-    # register pet.py
-    from . import pet
-
-    app.register_blueprint(pet.bp)
 
     # homepage index route
     @app.route('/')
     def index():
         return "My First App of Doom! <br> Home page"
 
+    # register pet.py
+    from . import pet
+    app.register_blueprint(pet.bp)
 
-    # pets index route
-    # @app.route('/pets')
-    # def pets(): 
-    #     return 'These are our pets available for adoption!'
-    
+
+    from . import fact
+    app.register_blueprint(fact.bp)
+
+        
     return app
